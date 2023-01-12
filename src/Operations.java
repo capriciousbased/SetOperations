e410dc528ae8cc5a1c3f44662d14633ee768a03c
capriciousbased <mdanisulhoquekhan@gmail.com>
2023-01-12 15:45:06 +0100

    add intersectionList

--- src/Operations.java
+++ src/Operations.java
@@ -3,12 +3,13 @@ import java.util.List;
 
 public class Operations {
 
-  public static void main(String[] args) {
-    unionList();
-  }
-
   static List<Integer> listA = List.of(1, 2, 3, 4, 5, 6);
   static List<Integer> listB = List.of(4, 5, 6, 7, 8, 9);
+
+  public static void main(String[] args) {
+    unionList();
+    intersectionList();
+  }
 
   public static void unionList() {
     List<Integer> result = new ArrayList<>(listA);
@@ -24,4 +25,17 @@ public class Operations {
     }
     System.out.println(result);
   }
+
+  public static void intersectionList() {
+    List<Integer> intersection = new ArrayList<>();
+    for (Integer element : listA) {
+      /*     We iterate through listA and check if the current element is also in listB using the contains() method. If it is, we add it to the intersection list. At the end of the iteration, the intersection list contains only the elements that were in both listA and listB. */
+      if (listB.contains(element)) {
+        intersection.add(element);
+      }
+    }
+
+    System.out.println("Intersection of both lists: " + intersection);
+  }
+
 }
